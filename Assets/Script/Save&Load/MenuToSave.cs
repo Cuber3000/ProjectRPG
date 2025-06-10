4cb73e4a24096261ffffe39efdd25c9476433f0d
Cuber3000 <videogamerz360@hotmail.com>
2025-06-10 11:39:22 +0800

    MenuToSave Script Has Save Values
    The Script has the default values which will be over written by the save script.

--- Assets/Script/Save&Load/MenuToSave.cs
+++ Assets/Script/Save&Load/MenuToSave.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using System;
 
-public class MenuToSave : MonoBehaviour
+// Serialized in order to save the quality, audio and keybinds settings.
+[Serializable]
+public class MenuToSave
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Audio")]
+    public float volume; // float due to slider being between 0 and 1
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Quality")]
+    public int quality; // 0 is low, 1 is medium, 3 is high. And these match the Uniti Quality setting.
+
+    [Header("KeyBinds")]
+    public string[] keyBindNames; // the name of the key, such as "Backward"
+
+    public string[] keyBindValues; // the value assigned to the key, such as "S" for "Backward"
 }
